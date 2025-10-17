4dc718611b606935da2f1fd9e3e693b9fc9103f2
Cazim Meskovic <meskovic0007@gmail.com>
2025-10-17 13:51:40 +0200

    radi sve

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Supabase URL ili Anon Key nisu pronađeni! Provjerite environment varijable.');
+  console.log('URL:', supabaseUrl);
+  console.log('ANON_KEY:', supabaseAnonKey ? 'Postoji' : 'Ne postoji');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
