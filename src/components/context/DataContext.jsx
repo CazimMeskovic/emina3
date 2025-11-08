5419b2adb3a0367864595e7ba691a7b0d0dc9c93
Cazim Meskovic <meskovic0007@gmail.com>
2025-11-08 10:38:00 +0100

    mergovo ono sa dohvatanjem podataka u main brans 8.11

---
+++ src/components/context/DataContext.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useState, useContext } from "react";
+
+const DataContext = createContext({
+  projects: null,
+  setProjects: () => {},
+});
+
+export const DataProvider = ({ children }) => {
+  const [projects, setProjects] = useState(null);
+
+  return (
+    <DataContext.Provider value={{ projects, setProjects }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export const useData = () => useContext(DataContext);
